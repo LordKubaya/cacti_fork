28e66f03d965ca3f7bca7c4b55f0fff3c5103f99
Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
2024-04-25 22:35:44 +0100

    feat(satp-hermes): fabric and besu bridge managers
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/satp-bridge/satp-bridge-manager.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/satp-bridge/satp-bridge-manager.ts
@@ -2,20 +2,74 @@
 // should inject satp gateway session data (having parameters/chains for transactions), and processes smart contract output
 
 export abstract class SATPBridgeManager {
+  /**
+   * Locks an asset on the blockchain
+   * @param sessionId
+   * @param assetId
+   * @returns transaction hash
+   **/
   public abstract lockAsset(
     sessionId: string,
     assetId: string,
   ): Promise<string>;
+  /**
+   * Unlocks an asset on the blockchain
+   * @param sessionId
+   * @param assetId
+   * @returns transaction hash
+   **/
   public abstract unlockAsset(
     sessionId: string,
     assetId: string,
   ): Promise<string>;
+  /**
+   * Mints an asset on the blockchain
+   * @param sessionId
+   * @param assetId
+   * @returns transaction hash
+   **/
   public abstract mintAsset(
     sessionId: string,
     assetId: string,
   ): Promise<string>;
+  /**
+   * Burns an asset on the blockchain
+   * @param sessionId
+   * @param assetId
+   * @returns transaction hash
+   **/
   public abstract burnAsset(
     sessionId: string,
     assetId: string,
   ): Promise<string>;
+  /**
+   * Assigns an asset to a recipient on the blockchain
+   * @param sessionId
+   * @param assetId
+   * @param recipient
+   * @returns transaction hash
+   **/
+  public abstract assignAsset(
+    sessionId: string,
+    assetId: string,
+    recipient: string,
+  ): Promise<string>;
+  /**
+   * Verifies the existence of an asset on the blockchain
+   * @param assetId
+   * @returns boolean
+   **/
+
+  //create-rollback
+  public abstract verifyAssetExistence(
+    assetId: string,
+  ): Promise<boolean | undefined>;
+  /**
+   * Verifies if an asset is locked on the blockchain
+   * @param assetId
+   * @returns boolean
+   **/
+  public abstract verifyLockAsset(
+    assetId: string,
+  ): Promise<boolean | undefined>;
 }
